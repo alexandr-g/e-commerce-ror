2996a7b8933c58319b84032db9da4f7e46514dea
Alexandr <alexgrischuk@gmail.com>
2015-08-14 20:41:22 +0600

    Add create products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,20 @@
 class ProductsController < ApplicationController
   def index
+    @product = Product.all
   end
 
   def new
+    @product = Product.new
   end
 
   def create
+      @product = Product.new(product_params)
+      if @product.save
+        redirect_to :action => 'index'
+      else
+        flash[:error]
+        render action: 'new'
+    end
   end
 
   def edit
@@ -16,4 +25,11 @@ class ProductsController < ApplicationController
 
   def show
   end
-end
+
+
+  private
+
+   def product_params
+     params.require(:product).permit(:name, :price, :description)
+   end
+ end
